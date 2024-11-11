b4f8131a29b2a8aa497c3de91856408702575435
ronitphilip <ronitpc1819@gmail.com>
2024-11-11 23:49:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Counter from './Counter'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <>
+      <Counter />
+    </>
+  )
+}
+
+export default App
